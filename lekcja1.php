4f5195b3abe609ab8c9380953b42275c46b96dcd
Fluing <Kuc.Mateusz@SZKOLA.ZST-RADOM.EDU.PL>
2026-09-14 08:28:11 +0200

    add index1.php

---
+++ lekcja1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?php
+    $a = 14;
+    echo $a;
+    echo "<br>";
+    echo -$a;
+    echo "<br>";
+    echo $a+ 3;
+    echo "<br>";
+    echo $a - 5;
+    echo "<br>";
+    echo $a * 2;
+    echo "<br>";
+    echo $a /5;
+    echo "<br>";
+    echo $a % 2;
+    echo "<br>";
+    echo $a ^ 3;
+    echo "<br>";
+    echo "Wartosc zmiennej $a wynosi: ".$a." i juz sie nie zmieni.";
+?>
+</body>
+</html>
